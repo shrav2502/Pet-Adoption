11fcbe7583fdc62aa7ccd850742ec7b591121346
shrav2502 <hebbarshravanthi@gmail.com>
2020-10-19 17:05:02 +0530

    modal added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import SearchParams from "./SearchParams";
 import "./style.css";
 import { Link, Router } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
-function App() {
+const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me! </Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me! </Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
-}
+};
 
 export default App;
